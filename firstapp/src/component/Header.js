4cf8b4527792c75de0ca04a720658ed3bf29988b
aakashdeveloper <ahanda205@gmail.com>
2021-03-12 21:56:51 +0530

    Added firstapp

--- firstapp/src/component/Header.js
+++ firstapp/src/component/Header.js
@@ -2,15 +2,29 @@ import React,{Component,Fragment} from 'react';
 import './Header.css';
 
 class Header extends Component{
+    constructor(){
+        super()
 
+        this.state={
+            title:'My React App',
+            keyword:'User Text Here'
+        }
+    }
+
+    handleChange= (event) => {
+        //console.log(event.target.value)
+        this.setState({keyword:event.target.value?event.target.value:'User Text Here'})
+        this.props.userText(event.target.value)
+    }
     render(){
+        //console.log("inside render")
         return(
             <Fragment>
                 <header>
-                    <div className="logo">Developer Funnel</div>
+                    <div className="logo">{this.state.title}</div>
                     <center>
-                        <input/>
-                        <div style={{color:'white'}}>User Text Here</div>
+                        <input onChange={this.handleChange}/>
+                        <div style={{color:'white'}}>{this.state.keyword}</div>
                     </center>
                 </header>
                 <hr/>
--- firstapp/src/component/Home.js
+++ firstapp/src/component/Home.js
@@ -1,16 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Header from './Header';
 import Footer from './Footer';
+import JSON from './db.json';
+import NewsDetails from './NewsDisplay'
 
-const Home =() => {
-    return(
-        <>
-            <Header/>
-            <h1>Hi from react</h1>
-            <h2>Nareshit</h2>
-            <Footer year="2021" month="March"/>
-        </>
-    )
+class Home extends Component{
+    constructor(){
+        super()
+
+        this.state={
+            news:JSON,
+            filtered:JSON
+        }
+    }
+
+    /*
+        var a = [5,3,35,6,45,3,42,34,3,24,35]
+        a.filter((data) => { return data>25})
+    */
+    filterNews=(keyword) => {
+        const output = this.state.news.filter((data) => {
+            return data.title.toLowerCase().indexOf(keyword.toLowerCase())>-1
+        })
+        this.setState({filtered:output})
+    }
+
+    render(){
+        //console.log(this.state.news)
+        return(
+            <>
+                <Header userText={(data) => {this.filterNews(data)}}/>
+                <NewsDetails newsdata={this.state.filtered}/>
+                <Footer year="2021" month="March"/>
+            </>
+        )
+    }
 }
+
 
 export default Home;
