eb128e3a6f5326ab0c7e5687e901bc84388e3bb8
aakashdeveloper <ahanda205@gmail.com>
2021-03-19 21:52:45 +0530

    add listing

--- thirdapp/src/component/Routing.js
+++ thirdapp/src/component/Routing.js
@@ -3,12 +3,16 @@ import Header from './Header';
 import Footer from './Footer';
 import {BrowserRouter, Route} from 'react-router-dom';
 import Home from './Home/Home';
+import ListingApi from './Listing/listingApi';
+import HotelDetails from './details/hotelDetails'
 
 const Routing = () => {
     return(
         <BrowserRouter>
             <Header/>
                 <Route exact path="/" component={Home}/>
+                <Route path="/list/:id" component={ListingApi}/>
+                <Route path="/details/:id" component={HotelDetails}/>
             <Footer/>
         </BrowserRouter>
     )
---
+++ thirdapp/src/component/details/hotelDetails.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class HotelDetails extends Component{
+    render(){
+        return(
+            <div>
+                Details
+            </div>
+        )
+    }
+}
+
+export default HotelDetails;
