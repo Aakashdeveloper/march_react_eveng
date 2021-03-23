faa30b6daa3e16b5320e0bda370ce36a1a7b7b6a
aakashdeveloper <ahanda205@gmail.com>
2021-03-23 21:47:33 +0530

    third complete

--- thirdapp/src/component/booking/placeBooking.js
+++ thirdapp/src/component/booking/placeBooking.js
@@ -1,8 +1,10 @@
 import React,{Component} from 'react';
 
+const bookingUrl = "http://localhost:9080/bookings";
+
 class PlaceOrder extends Component{
-    constructor(){
-        super()
+    constructor(props){
+        super(props)
 
         this.state={
             id:Math.floor(Math.random()*1000000),
@@ -13,9 +15,62 @@ class PlaceOrder extends Component{
         }
     }
 
+    handleChange  = (event) => {
+        this.setState({[event.target.name]:event.target.value})
+    }
+
+    handleSubmit = () => {
+        console.log(this.state)
+        fetch(bookingUrl,{
+            method:'POST',
+            headers:{
+                'Accept':'application/json',
+                'Content-Type':'application/json'
+            },
+            body:JSON.stringify(this.state)
+        })
+        .then(this.props.history.push('/viewBooking'))
+    }
     render(){
         return(
-            <div></div>
+            <div className="container">
+                <div className="panel panel-info">
+                    <div className="panel-heading">
+                        Place Booking
+                    </div>
+                    <div className="panel-body">
+                        <div className="form-group">
+                            <label>Order Id</label>
+                            <input name="id" value={this.state.id}
+                            readOnly className="form-control"/>
+                        </div>
+                        <div className="form-group">
+                            <label>Hotel Name</label>
+                            <input name="hotel_name" value={this.state.hotel_name}
+                            readOnly className="form-control"/>
+                        </div>
+                        <div className="form-group">
+                            <label>Cost</label>
+                            <input name="cost" value={this.state.cost}
+                            readOnly className="form-control"/>
+                        </div>
+                        <div className="form-group">
+                            <label>Name</label>
+                            <input name="name" value={this.state.name}
+                             className="form-control" onChange={this.handleChange}/>
+                        </div>
+                        <div className="form-group">
+                            <label>Phone</label>
+                            <input name="phone" value={this.state.phone}
+                             className="form-control" onChange={this.handleChange}/>
+                        </div>
+                        <button className="btn btn-success"
+                        onClick={this.handleSubmit}>
+                            Place Booking
+                        </button>
+                    </div>
+                </div>
+            </div>
         )
     }
 }
